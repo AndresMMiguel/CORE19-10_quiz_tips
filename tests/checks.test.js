d844fbef520b930a783a91b0f66987854bab2236
practicas-ging <practicas-ging@mail.com>
2019-04-25 09:54:00 +0200

    Updated 10_quiz_tips

--- tests/checks.test.js
+++ tests/checks.test.js
@@ -28,7 +28,6 @@ let url = "http://localhost:5000";
 
 // HELPERS
 const timeout = ms => new Promise(res => setTimeout(res, ms));
-let server = null;
 
 //TESTS
 describe("CORE19-10_quiz_tips", function () {
@@ -40,11 +39,11 @@ describe("CORE19-10_quiz_tips", function () {
         this.score = 0;
         this.msg_ok = `Found the directory '${path_assignment}'`;
         this.msg_err = `Couldn't find the directory '${path_assignment}'`;
-        const [error_path, path_ok] = await to(fs.pathExists(path_assignment));
+        const [error_path, _] = await to(fs.pathExists(path_assignment));
         if (error_path) {
             error_critical = this.msg_err;
         }
-        path_ok.should.be.equal(true);
+        should.not.exist(error_critical);
     });
 
     it('', async function () {
@@ -52,7 +51,7 @@ describe("CORE19-10_quiz_tips", function () {
         this.score = 0;
         if (error_critical) {
             this.msg_err = error_critical;
-            should.not.exist(error_critical);
+
         } else {
             this.msg_ok = "Dependencies installed successfully";
             this.msg_err = "Error installing dependencies";
@@ -62,8 +61,8 @@ describe("CORE19-10_quiz_tips", function () {
                 this.msg_err = `The file '${path_json}' has not been found`;
                 error_critical = this.msg_err;
             }
-            json_ok.should.be.equal(true);
         }
+        should.not.exist(error_critical);
     });
 
     it('', async function () {
@@ -71,7 +70,7 @@ describe("CORE19-10_quiz_tips", function () {
         this.score = 0;
         if (error_critical) {
             this.msg_err = error_critical;
-            should.not.exist(error_critical);
+
         } else {
             this.msg_ok = "The 'package.json' file has the right format";
             this.msg_err = "Error parsing the 'package.json' file";
@@ -85,8 +84,8 @@ describe("CORE19-10_quiz_tips", function () {
             if (!is_json) {
                 error_critical = this.msg_err;
             }
-            is_json.should.be.equal(true);
         }
+        should.not.exist(error_critical);
     });
 
     it('', async function () {
@@ -95,7 +94,6 @@ describe("CORE19-10_quiz_tips", function () {
         this.score = 0;
         if (error_critical) {
             this.msg_err = error_critical;
-            should.not.exist(error_critical);
         } else {
             this.msg_ok = "Dependencies installed successfully";
             this.msg_err = "Error installing dependencies";
@@ -108,8 +106,8 @@ describe("CORE19-10_quiz_tips", function () {
                 this.msg_err = "Error installing dependencies: " + error_deps;
                 error_critical = this.msg_err;
             }
-            should.not.exist(error_critical);
         }
+        should.not.exist(error_critical);
     });
 
     it('', async function () {
@@ -117,7 +115,6 @@ describe("CORE19-10_quiz_tips", function () {
         this.score = 0;
         if (error_critical) {
             this.msg_err = error_critical;
-            should.not.exist(error_critical);
         } else {
             this.msg_ok = "'quizzes.sqlite' replaced successfully";
             this.msg_err = "Error replacing 'quizzes.sqlite'";
@@ -130,8 +127,8 @@ describe("CORE19-10_quiz_tips", function () {
             if (error_deps) {
                 this.msg_err = "Error copying the answers file: " + error_deps;
             }
-            should.not.exist(error_deps);
         }
+        should.not.exist(error_critical);
     });
 
     it('', async function () {
@@ -140,10 +137,9 @@ describe("CORE19-10_quiz_tips", function () {
         this.score = 1;
         if (error_critical) {
             this.msg_err = error_critical;
-            should.not.exist(error_critical);
         } else {
             this.msg_ok = `'${expected}' has been launched correctly`;
-            server = spawn("node", [expected], {cwd: path_assignment});
+            let server = spawn("node", [expected], {cwd: path_assignment});
             let error_launch = "";
             server.on('error', function (data) {
                 error_launch += data
@@ -152,10 +148,9 @@ describe("CORE19-10_quiz_tips", function () {
             this.msg_err = `Error launching '${expected}'<<\n\t\t\tReceived: ${error_launch}`;
             if (error_launch.length) {
                 error_critical = this.msg_err;
-                should.not.exist(error_critical);
             }
-            error_launch.should.be.equal("");
         }
+        should.not.exist(error_critical);
     });
 
     it('', async function () {
@@ -167,13 +162,8 @@ describe("CORE19-10_quiz_tips", function () {
             should.not.exist(error_critical);
         } else {
             this.msg_ok = `Server responded at ${expected}`;
-            let error_nav;
             [error_nav, resp] = await to(browser.visit(expected));
-            if (error_nav) {
-                this.msg_err = `Server not responding at ${expected}\nError:${error_nav}\nReceived:${browser.text('body')}`;
-                error_critical = this.msg_err;
-                should.not.exist(error_critical);
-            }
+            this.msg_err = `Server not responding at ${expected}\nError:${error_nav}\nReceived:${browser.text('body')}`;
             should.not.exist(error_nav);
         }
     });
@@ -187,13 +177,8 @@ describe("CORE19-10_quiz_tips", function () {
             should.not.exist(error_critical);
         } else {
             this.msg_ok = `Server responded at ${expected}`;
-            let error_nav;
             [error_nav, resp] = await to(browser.visit(expected));
-            if (error_nav) {
-                this.msg_err = `Server not responding at ${expected}\nError:${error_nav}\nReceived:${browser.text('body')}`;
-                error_critical = this.msg_err;
-                should.not.exist(error_critical);
-            }
+            this.msg_err = `Server not responding at ${expected}\nError:${error_nav}\nReceived:${browser.text('body')}`;
             should.not.exist(error_nav);
         }
     });
@@ -207,7 +192,6 @@ describe("CORE19-10_quiz_tips", function () {
             should.not.exist(error_critical);
         } else {
             this.msg_ok = `Server responded at ${expected}`;
-            let error_nav;
             [error_nav, resp] = await to(browser.visit(expected));
             this.msg_err = `Server not responding at ${expected}\nError:${error_nav}\nReceived:${browser.text('body')}`;
             should.not.exist(error_nav);
@@ -224,7 +208,6 @@ describe("CORE19-10_quiz_tips", function () {
             should.not.exist(error_critical);
         } else {
             this.msg_ok = `Found the anonymous user '${expected}' at ${url}`;
-            let error_nav;
             [error_nav, resp] = await to(browser.visit(url));
             this.msg_err = `Server not responding at ${url}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
             should.not.exist(error_nav);
@@ -243,7 +226,6 @@ describe("CORE19-10_quiz_tips", function () {
             should.not.exist(error_critical);
         } else {
             this.msg_ok = `Found the registered user '${expected}' at ${url}`;
-            let error_nav;
             [error_nav, resp] = await to(browser.visit(url));
             this.msg_err = `Server not responding at ${url}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
             should.not.exist(error_nav);
@@ -256,66 +238,69 @@ describe("CORE19-10_quiz_tips", function () {
         const expected = 'meaning';
         const user = 'registrado';
         let url_login = url + "/session";
-        let url = url + '/quizzes/5';
+        let myurl = url + '/quizzes/5';
         this.name = `12: Checking that the server shows clues to the registered user...`;
         this.score = 1;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            this.msg_ok = `Found clues for the registered user '${user}' at ${url}`;
+            this.msg_ok = `Found clues for the registered user '${user}' at ${myurl}`;
             let error_nav;
             [error_nav, resp] = await to(browser.visit(url_login));
             this.msg_err = `Could not open login url '${url_login}'\n\t\t\tError: >>${error_nav}`;
+            should.not.exist(error_nav);
             [error_nav, resp] = await to(browser.assert.element('input[id=login]'));
             this.msg_err = `Could not find login input at '${url_login}'\n\t\t\tError: >>${error_nav}`;
+            should.not.exist(error_nav);
             [error_nav, resp] = await to(browser.assert.element('input[id=password]'));
             this.msg_err = `Could not find login password at '${url_login}'\n\t\t\tError: >>${error_nav}`;
+            should.not.exist(error_nav);
             [error_nav, resp] = await to(browser.fill('input[id=login]', user));
             this.msg_err = `Could not fill login input at '${url_login}'\n\t\t\tError: >>${error_nav}`;
+            should.not.exist(error_nav);
             [error_nav, resp] = await to(browser.fill('input[id=password]', user));
             this.msg_err = `Could not fill login password at '${url_login}'\n\t\t\tError: >>${error_nav}`;
             [error_nav, resp] = await to(browser.pressButton("[name=commit]"));
-            this.msg_err = `Could not send login form at '${url_login}'\n\t\t\tError: >>${error_nav}`;
-            [error_nav, resp] = await to(browser.visit(url));
-            this.msg_err = `Server not responding at ${url}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
             should.not.exist(error_nav);
-            this.msg_err = `Clues for the registered user not found at ${url}\n\t\tReceived:${browser.text('body')}`;
+            this.msg_err = `Could not send login form at '${url_login}'\n\t\t\tError: >>${error_nav}`;
+            [error_nav, resp] = await to(browser.visit(myurl));
+            this.msg_err = `Server not responding at ${myurl}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
+            should.not.exist(error_nav);
+            this.msg_err = `Clues for the registered user not found at ${myurl}\n\t\tReceived:${browser.text('body')}`;
             Utils.search(expected, browser.text('body')).should.be.equal(true);
         }
     });
 
     it('', async function () {
         const expected = 'registrado';
-        let url = url + '/quizzes/5';
+        let myurl = url + '/quizzes/5';
         this.name = `13: Checking that the server shows the registered user in the clues...`;
         this.score = 1;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            this.msg_ok = `Found the registered user '${expected}' in the clues  at ${url}`;
-            let error_nav;
-            [error_nav, resp] = await to(browser.visit(url));
-            this.msg_err = `Server not responding at ${url}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
+            this.msg_ok = `Found the registered user '${expected}' in the clues  at ${myurl}`;
+            [error_nav, resp] = await to(browser.visit(myurl));
+            this.msg_err = `Server not responding at ${myurl}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
             should.not.exist(error_nav);
-            this.msg_err = `Registered user not found in the clues at ${url}\n\t\tReceived:${browser.text('body')}`;
+            this.msg_err = `Registered user not found in the clues at ${myurl}\n\t\tReceived:${browser.text('body')}`;
             Utils.search(expected, browser.text('body')).should.be.equal(true);
         }
     });
 
     it('', async function () {
-        let url = url + '/quizzes/randomcheck/1?answer=NOK';
-        this.name = `10: Checking that the server accepts wrong answers at ${url}...`;
+        const expected = url + '/quizzes/randomcheck/1?answer=NOK';
+        this.name = `10: Checking that the server accepts wrong answers at ${expected}...`;
         this.score = 1;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            this.msg_ok = `Wrong answer sent successfully to ${url}\n\t\tReceived:${browser.text('body')}`;
-            let error_nav;
-            [error_nav, resp] = await to(browser.visit(url));
-            this.msg_err = `Server not responding at ${url}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
+            this.msg_ok = `Wrong answer sent successfully to ${expected}\n\t\tReceived:${browser.text('body')}`;
+            [error_nav, resp] = await to(browser.visit(expected));
+            this.msg_err = `Server not responding at ${expected}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
             should.not.exist(error_nav);
         }
     });
@@ -323,34 +308,31 @@ describe("CORE19-10_quiz_tips", function () {
 
     it('', async function () {
         const expected = '0';
-        let url = url + '/quizzes/randomcheck/1?answer=NOK';
-        this.name = `11: Checking the score at ${url}...`;
+        let myurl = url + '/quizzes/randomcheck/1?answer=NOK';
+        this.name = `11: Checking the score at ${myurl}...`;
         this.score = 1;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            this.msg_ok = `Found the right score '${expected}' at ${url}`;
-            let error_nav;
-            [error_nav, resp] = await to(browser.visit(url));
-            this.msg_err = `Server not responding at ${url}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
-            should.not.exist(error_nav);
+            this.msg_ok = `Found the right score '${expected}' at ${myurl}`;
+            this.msg_err = `Score '${expected}' not found at ${myurl}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
             Utils.search(expected, browser.text('body')).should.be.equal(true);
         }
     });
 
     it('', async function () {
-        let url = url + '/randomcheck/1?answer=OK';
-        this.name = `12: Checking that the server accepts right answers at ${url}...`;
+        const expected = url + '/randomcheck/1?answer=OK';
+        this.name = `12: Checking that the server accepts right answers at ${expected}...`;
         this.score = 1;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            this.msg_ok = `Right answer sent successfully to ${url}\n\t\tReceived:${browser.text('body')}`;
+            this.msg_ok = `Right answer sent successfully to ${expected}\n\t\tReceived:${browser.text('body')}`;
             let error_nav;
-            [error_nav, resp] = await to(browser.visit(url));
-            this.msg_err = `Server not responding at ${url}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
+            [error_nav, resp] = await to(browser.visit(expected));
+            this.msg_err = `Server not responding at ${expected}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
             should.not.exist(error_nav);
         }
     });
@@ -358,19 +340,15 @@ describe("CORE19-10_quiz_tips", function () {
 
     it('', async function () {
         const expected = '1';
-        let url = url + '/randomcheck/1?answer=OK';
-        this.name = `13: Checking the score at ${url}...`;
+        let myurl = url + '/randomcheck/1?answer=OK';
+        this.name = `13: Checking the score at ${myurl}...`;
         this.score = 2;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            this.msg_ok = `Found the right score '${expected}' at ${url}`;
-            let error_nav;
-            [error_nav, resp] = await to(browser.visit(url));
-            this.msg_err = `Server not responding at ${url}\n\t\tError:${error_nav}\n\t\tReceived:${browser.text('body')}`;
-            should.not.exist(error_nav);
-            this.msg_err = `The right score '${expected}' has not been found at ${url}\n\t\tReceived:${browser.text('body')}`;
+            this.msg_ok = `Found the right score '${expected}' at ${myurl}`;
+            this.msg_err = `The right score '${expected}' has not been found at ${myurl}\n\t\tReceived:${browser.text('body')}`;
             Utils.search(expected, browser.text('body')).should.be.equal(true);
         }
     });
