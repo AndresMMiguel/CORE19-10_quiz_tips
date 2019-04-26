ae0ff462323f6eb6be57f750b6b445b78cfb38f3
practicas-ging <practicas-ging@mail.com>
2019-04-26 13:07:18 +0200

    Updated 10_quiz_tips

--- tests/checks.test.js
+++ tests/checks.test.js
@@ -16,15 +16,15 @@ const Browser = require('zombie');
 let error_critical = null;
 
 // CONSTANTS
-const T_WAIT = 2 * 60; // Server boot time
+const T_WAIT = 60; // Server boot time (seconds)
 const T_TEST = 5 * 60; // Time between tests (seconds)
 const path_assignment = path.resolve(path.join(__dirname, "../quiz_2019"));
 const path_json = path.join(path_assignment, 'package.json');
-const quizzes_orig = path.join(path_assignment, 'quizzes.sqlite');
-const quizzes_back = path.join(path_assignment, 'quizzes.original.sqlite');
-const quizzes_test = path.join(__dirname, 'quizzes.sqlite');
+const quizzes_orig = path.join(path_assignment, 'quiz.sqlite');
+const quizzes_back = path.join(path_assignment, 'quiz.original.sqlite');
+const quizzes_test = path.join(__dirname, 'quiz.sqlite');
 const browser = new Browser();
-const url = "http://localhost:5000";
+const url = "http://localhost:3000";
 
 // HELPERS
 const timeout = ms => new Promise(res => setTimeout(res, ms));
